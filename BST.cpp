835cf18f307b1bca1404e308652aa74a23fe821b
Kushagra Sahu <kushagrasahu.ed@gmail.com>
2016-07-30 18:04:55 +0530

    BST init

---
+++ BST.cpp
@@ -0,0 +1,202 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct node{
+	int data;
+	node *left;
+	node *right;
+};
+
+bool isLeafNode(node *root){
+	if(!root->left && !root->right){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+//Recursion
+node* insertInBST_1(node *&root, int value){
+	if(!root){
+		root=new node;
+		root->data=value;
+		root->left=root->right=NULL;
+		return root;
+	}
+	if(root->data > value){
+		root->left=insertInBST_1(root->left,value);
+	}
+	else{
+		root->right=insertInBST_1(root->right,value);
+	}
+	return root;
+}
+
+//Method2
+void insertInBST_2(node *&root, int value){
+	node* new_node= new node;
+	new_node->data=value;
+	new_node->left=new_node->right=NULL;
+	if(!root){
+		root=new_node;
+		return;
+	}
+	node* parent, *temp=root;
+	while(temp){
+		parent=temp;
+		if(temp->data < value){
+			temp=temp->right;
+		}
+		else{
+			temp=temp->left;
+		}
+	}
+	if(parent->data < value){
+		parent->right=new_node;
+	}
+	else{
+		parent->left=new_node;
+	}
+}
+
+node *findMinNode(node *root){
+	while(root->left){
+		root=root->left;
+	}
+	return root;
+}
+
+node *findMaxNode(node *root){
+	while(root->right){
+		root=root->right;
+	}
+	return root;
+}
+
+node *findNodeInBST(node *root,int data1){
+	while(root){
+		if(root->data < data1){
+			root=root->right;
+		}
+		else if(root->data > data1){
+			root=root->left;
+		}
+		else{
+			return root;
+		}
+	}
+	return NULL;
+}
+
+node *deleteNodeFromBST(node *root, int value){
+	if(root->data > value){
+		root->left = deleteNodeFromBST(root, value);
+	}
+	else if(root->data < value){
+		root->right = deleteNodeFromBST(root, value);
+	}
+	else{
+		if(!root->left && !root->right){
+			delete root;
+			return NULL;
+		}
+		else if(root->left && !root->right){
+			node *temp = root->left;
+			delete root;
+			return temp;
+		}
+		else if(!root->left && root->right){
+			node *temp = root->right;
+			delete root;
+			return temp;
+		}
+		else{
+			node *min = findMinNode(root->right);
+			root->data = min->data;
+			root->right = deleteNodeFromBST(root->right, min->data);
+			return root;
+		}
+	}
+	return root;
+}
+
+void printLevelOrderBST(node *root){
+	if(!root){
+		return;
+	}
+	queue<node *> Q;
+	Q.push(root);
+	Q.push(NULL);
+	while(!Q.empty()){
+		node *temp=Q.front();
+		Q.pop();
+		if(!temp){
+			if(!Q.empty()){
+				cout << endl;
+				Q.push(NULL);
+			}
+		}
+		else{
+			cout << temp->data << " ";
+			if(temp->left){
+				Q.push(temp->left);
+			}
+			if(temp->right){
+				Q.push(temp->right);
+			}
+		}
+	}
+	cout << endl;
+}
+
+node *findInorderSuccessorOfNode(node *root, int value){
+	node *targetNode = findNodeInBST(root, value);
+	node *inorder_successor=NULL;
+	if(targetNode->right){
+		return findMinNode(targetNode->right);
+	}
+	else{
+		while(root){
+			if(root->data > value){
+				inorder_successor = root;
+				root=root->left;
+			}
+			else if(root->data < value){
+				root=root->right;
+			}
+			else{
+				break;
+			}
+		}
+		return inorder_successor;
+	}
+}
+
+bool isTreeBST(node *root){
+	bool temp = true;
+	if(root->left){
+		if(root->data >= root->left->data){
+			temp = isTreeBST(root->left);
+		}
+		else{
+			return false;
+		}
+	}
+	if(temp){
+		if(root->right){
+			if(root->data < root->right->data){
+				temp = isTreeBST(root->right);
+			}
+			else{
+				return false;
+			}
+		}
+	}
+	return temp;
+}
+
+int main(){
+	//All Functions-> DRY RUN.
+}
